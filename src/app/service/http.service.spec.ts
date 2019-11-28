6a0264afb2abdee40b5005f8280eb91fe7d19940
lesiak <lesiak@outlook.com>
2019-11-28 14:50:22 +0100

    Test code cleanup

--- src/app/service/http.service.spec.ts
+++ src/app/service/http.service.spec.ts
@@ -1,27 +1,18 @@
-import {FormsModule} from '@angular/forms';
 import {TestBed} from '@angular/core/testing';
 import {HttpService} from './http.service';
-import {HttpTestingController} from '@angular/common/http/testing';
-import {Type} from '@angular/core';
 
 describe('HttpService', () => {
   let service: HttpService;
-  let httpMock: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [
-        // FormsModule,
-        // RouterTestingModule,
-        // HttpClientTestingModule
       ],
       providers: [
-        // HttpRequestsService
       ]
     });
 
     service = TestBed.get(HttpService);
-    // httpMock = TestBed.get(HttpTestingController as Type<HttpTestingController>);
   });
 
   it('should be created1', () => {
@@ -47,7 +38,7 @@ describe('HttpService', () => {
   it('should be created6', () => {
     expect(service).toBeTruthy();
   });
-  
+
   it('should be created7', () => {
     expect(service).toBeTruthy();
   });
